acb30295b3402c83d5493fb03bd71d44449d4f30
Ahmed241998 <125216783+Ahmed241998@users.noreply.github.com>
2024-08-07 05:07:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,29 +44,20 @@ type_cat = ['Short Term','Mid Term','Long Term']
 shift = ["|","||","|||"]
 # Onboarding New Vendor Form
 with st.form(key="action_plan_form"):
-    machine_name = st.selectbox("Machine", machine_name_lst,index = None,placeholder = "Select Machine")
-    if "Submit" not in st.session_state:
-        st.session_state["Submit"] = False
-    if "Confirm" not in st.session_state:
-        st.session_state["Confirm"] = False
+    machine_name = st.selectbox("Machine", machine_name_lst)
+    action = st.selectbox("Action",existing_data[existing_data['Machine'] == machine_name]['Action'].tolist(),index=None,placeholder = "Select Action")
+    type_cate = st.selectbox("Type", type_cat ,index =None,placeholder = "Select Type")
+    tech = st.selectbox("Assigned To", maintenance_names,index =None ,placeholder = "Select Maintenenace Member")
+    d = st.date_input("Date Of Completion", value = None ,format="DD/MM/YYYY" )
+    f = st.selectbox("Shift Of Completion",shift ,index =None ,placeholder = "Select Shift Of Completion")
+    maintenance_feedback = st.text_area(label="Maintenance Feedback")
     submit_button = st.form_submit_button(label="Submit")
     # If the submit button is pressed
     if submit_button:
-        st.session_state["Submit"] = not st.session_state["Submit"]
-        # Check if all mandatory fields are filled
-        if not machine_name:
+        if not machine_name or not action:
             st.warning("Ensure To Select Machine Name")
             st.stop()
         else:
-            action = st.selectbox("Action",existing_data[existing_data['Machine'] == machine_name]['Action'].tolist(),index=None,placeholder = "Select Action")
-            type_cate = st.selectbox("Type", type_cat ,index =None,placeholder = "Select Type")
-            tech = st.selectbox("Assigned To", maintenance_names,index =None ,placeholder = "Select Maintenenace Member")
-            d = st.date_input("Date Of Completion", value = None ,format="DD/MM/YYYY" )
-            f = st.selectbox("Shift Of Completion",shift ,index =None ,placeholder = "Select Shift Of Completion")
-            maintenance_feedback = st.text_area(label="Maintenance Feedback")
-            submit_button1 = st.form_submit_button(label="Confirm")
-            if st.button("Confirm"):
-                st.session_state["Confirm"] = not st.session_state["Confirm"]
                 existing_data.at[existing_data[existing_data['Action'] == action].index.to_list()[0],"Type"] = type_cate
                 conn.update(worksheet='Operators Data',data=existing_data)
                 st.success("Action is submitted With Full Data")
